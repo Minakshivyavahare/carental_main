d226a0a57370eef164f44c126c1bb926764fdf0d
Minakshi Vyavahare <130536242+Minakshivyavahare@users.noreply.github.com>
2025-05-16 14:14:59 +0530

    change base_url

--- src/features/AllCar/carService.js
+++ src/features/AllCar/carService.js
@@ -1,8 +1,17 @@
 import axios from "axios"
 
+const base_url = "https://car-rental-app-5d25.onrender.com"
+
 const fetchAllCar = async(page = 1) => {
-    const response = await axios.get(`/api/car?page=${page}`)
+    
+    try {
+        const response = await axios.get(base_url + `/api/car?page=${page}`)
+        console.log("response cars",response.data)
     return response.data
+    } catch (error) {
+        console.log("all cars", error)
+    }
+    
 }
 
 const fetchSingleCar = async(id) => {
